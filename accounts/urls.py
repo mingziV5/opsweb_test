c9a4007c04ce81f72de69c693d29e1b8f88d3ff2
mingziV5 <ming_v5@163.com>
2017-08-18 16:15:52 +0000

    完善类试图

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     #url(r'^login/$', views.user_login, name='user_login'),
     #url(r'^logout/$', views.user_logout, name='user_logout'),
     #view类视图
-    url(r'^login/$', user.views.UserLoginView.as_view(), name='user_login'),
-    url(r'^logout/$', user.views.UserLogoutView.as_view(), name='user_logout'),
+    #url(r'^login/$', user.views.UserLoginView.as_view(), name='user_login'),
+    #url(r'^logout/$', user.views.UserLogoutView.as_view(), name='user_logout'),
+    #template类视图
+    url(r'^login/$', user.views.UserLoginTplView.as_view(), name='user_login'),
+    url(r'^logout/$', user.views.UserlogoutTplView.as_view(), name='user_logout'),
     #url(r'user/list/$', views.user_list_view, name='user_list'),
     #类视图，as_view()方法
     #url(r'user/list/$', views.UserListView.as_view(), name='user_list'),
--- accounts/user/views.py
+++ accounts/user/views.py
@@ -30,25 +30,23 @@ class UserLogoutView(View):
         logout(request)
         return HttpResponseRedirect(reverse('user_login'))
 
-'''
-TemplateView Method Not Allowed (POST)
 class UserLoginTplView(TemplateView):
     template_name = 'public/login.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(UserLoginTplView, self).get_context_data(**kwargs)
-        if self.request.method == 'POST':
-            username = self.request.POST.get('username', None)
-            password = self.request.POST.get('password', None)
-            user = authenticate(username = username, password = password)
-            if user:
-                login(self.request, user)
-                context['status'] = 0
-                context['next_url'] = self.request.GET.get('next') if self.request.GET.get('next', None) else '/'
-            else:
-                context['status'] = 1
-                context['errmsg'] = '用户名密码错误'
-            return context
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        user = authenticate(username=username, password=password)
+        response = {'status': 0}
+        if user:
+            login(request, user)
+            response['next_url'] = request.GET.get('next') if request.GET.get('next', None) else '/'
         else:
-            return context
-'''
+            response['status'] = 1
+            response['errmsg'] = '用户名密码错误'
+        return JsonResponse(response)
+
+class UserlogoutTplView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect(reverse('user_login'))

540e933bde1209e1ff8d34549bcf423c85a6fecf
mingziV5 <ming_v5@163.com>
2017-08-31 15:27:37 +0000

    修改添加idc,分开ajax与传统页面提交

--- resources/views.py
+++ resources/views.py
@@ -1,9 +1,12 @@
 from django.views.generic import TemplateView
 from django.shortcuts import redirect, reverse
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from resources.models import Idc
+
+import traceback
 # Create your views here.
 
+#通过页面submit提交
 class CreateIdcView(TemplateView):
     template_name = "idc/add_idc.html"
 
@@ -13,17 +16,19 @@ class CreateIdcView(TemplateView):
         add_idc_form = request.POST.copy()
         add_idc_form.pop('csrfmiddlewaretoken')
         add_idc_dict = add_idc_form.dict()
-        response = {}
         try:
             idc = Idc(**add_idc_dict)
             idc.save()
             return redirect("success", next="user_list")
         except:
-            response['status'] = 1
-            response['errmsg'] = '添加idc信息错误'
-            return JsonResponse(response)
+            print(traceback.format_exc())
+            errmsg = '添加idc信息错误'
+            return redirect("error", next="idc_add", msg=errmsg)
         #操作成功
 
         #操作失败
         #return redirect("error", next="idc_add", msg="error")
         #return HttpResponse("")
+
+#TODO
+#通过ajax提交

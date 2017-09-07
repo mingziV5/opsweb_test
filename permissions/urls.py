211522f411b53900df172ce707c3900d68169667
mingziV5 <ming_v5@163.com>
2017-09-07 01:58:12 +0000

    完成权限搜索

--- permissions/urls.py
+++ permissions/urls.py
@@ -4,5 +4,4 @@ from permissions import views
 urlpatterns = [
     url(r'^list/$', views.PermissionListView.as_view(), name="permission_list"),
     url(r'^add/$', views.PermissionAddView.as_view(), name="permission_add"),
-    url(r'^search/$',views.PermissionSearchView.as_view(), name="permission_search"),
 ]
--- permissions/views.py
+++ permissions/views.py
@@ -16,7 +16,13 @@ class PermissionListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         queryset = super(PermissionListView, self).get_queryset()
+        #获得搜索内容
         queryset = Permission.objects.all()
+        search_value = self.request.GET.get('search_value', None)
+        if search_value:
+            #Q连表查询 |：OR ~: != &: AND
+            queryset = queryset.filter(Q(codename__contains=search_value)|Q(content_type_id__model__contains=search_value))
+            #print(queryset.filter(Q(codename__contains=search_value)|Q(content_type_id__model__contains=search_value)).query)
         return queryset
 
     def get_page_range(self, page_obj):
@@ -27,11 +33,20 @@ class PermissionListView(LoginRequiredMixin, ListView):
             start = 1
         if end > page_obj.paginator.num_pages:
             end = page_obj.paginator.num_pages
-        return range(start, end + 1)
+        return range(start, end+1)
 
     def get_context_data(self, **kwargs):
         context = super(PermissionListView, self).get_context_data(**kwargs)
         context['page_range_obj'] = self.get_page_range(context['page_obj'])
+        #处理查询条件
+        search_data = self.request.GET.copy()
+        try:
+            search_data.pop("page")
+        except:
+            pass
+        context.update(search_data.dict())
+        context['search_data'] = "&" + search_data.urlencode()
+        print(context)
         return context
 
 class PermissionAddView(LoginRequiredMixin, TemplateView):
@@ -67,47 +82,6 @@ class PermissionAddView(LoginRequiredMixin, TemplateView):
             print(e)
             msg = "添加权限出错"
             return redirect("error", next="permission_add", msg=msg)
-
-class PermissionSearchView(LoginRequiredMixin, ListView):
-    template_name = 'permissions_list.html'
-    model = Permission
-    paginate_by = 10
-    before_range_num = 4
-    after_range_num = 5
-    ordering = 'id'
-
-    def get_queryset(self):
-        queryset = super(PermissionSearchView, self).get_queryset()
-        #获得搜索内容
-        search_value = self.request.GET.get('search_value', None)
-        if search_value:
-            queryset = queryset.filter(codename__contains=search_value)
-            print(connection.queries)
-        return queryset
-
-    def get_page_range(self, page_obj):
-        current_index = page_obj.number
-        start = current_index - self.before_range_num
-        end = current_index + self.after_range_num
-        if start <= 0:
-            start = 1
-        if end > page_obj.paginator.num_pages:
-            end = page_obj.paginator.num_pages
-        return range(start, end+1)
-
-    def get_context_data(self, **kwargs):
-        context = super(PermissionSearchView, self).get_context_data(**kwargs)
-        context['page_range_obj'] = self.get_page_range(context['page_obj'])
-
-        #处理查询条件
-        search_data = self.request.GET.copy()
-        try:
-            search_data.pop("page")
-        except:
-            pass
-        context.update(search_data.dict())
-        context['search_data'] = "&" + search_data.urlencode()
-        return context
 
 
 
